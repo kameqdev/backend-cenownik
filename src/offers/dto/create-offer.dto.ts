18116a8e246e592edb4355d236f84a4db86e4b52
Kameq <radek.rbb@gmail.com>
2026-01-04 01:10:39 +0100

    feat: add global validation pipe and dto validation

--- src/offers/dto/create-offer.dto.ts
+++ src/offers/dto/create-offer.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsNumber, IsUUID, IsUrl } from "class-validator";
+
 export class CreateOfferDto {
+  @IsUrl()
+  @IsNotEmpty()
   url: string;
+
+  @IsNumber()
+  @IsNotEmpty()
   targetPrice: number;
+
+  @IsUUID()
+  @IsNotEmpty()
   userId: string;
 }
